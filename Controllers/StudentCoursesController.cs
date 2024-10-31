232db0f005ee177463b8693da9266b969665dc3a
aldorQlok <aldor.besher@qlok.se>
2024-10-31 03:42:03 +0100

    added final controller

---
+++ Controllers/StudentCoursesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FluentAPI.Data;
+using FluentAPI.Models;
+
+namespace FluentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentCoursesController : ControllerBase
+    {
+        private readonly FluentAPIContext _context;
+
+        public StudentCoursesController(FluentAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/StudentCourses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentCourse>>> GetStudentCourses()
+        {
+            return await _context.StudentCourses.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StudentCourse>> GetStudentCourse(int id)
+        {
+            var studentCourse = await _context.StudentCourses.FindAsync(id);
+
+            if (studentCourse == null)
+            {
+                return NotFound();
+            }
+
+            return studentCourse;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<StudentCourse>> PostStudentCourse(StudentCourse studentCourse)
+        {
+            _context.StudentCourses.Add(studentCourse);
+            await _context.SaveChangesAsync();
+
+            return Ok(studentCourse);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudentCourse(int id)
+        {
+            var studentCourse = await _context.StudentCourses.FindAsync(id);
+            if (studentCourse == null)
+            {
+                return NotFound();
+            }
+
+            _context.StudentCourses.Remove(studentCourse);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
